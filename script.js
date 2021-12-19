47dfb9b1db549fd6af803b20956fc36cad6ace6d
Johan LEPINAY <johan.lepinay@yanipel.com>
2021-12-20 01:17:37 +0400

    Add video

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import Swiper from 'swiper/swiper-bundle.esm.js';
+import 'swiper/swiper-bundle.css';
+
 // Navbar
 let lastScrollTop = 0;
 navbar = document.getElementById('navbar');
@@ -52,6 +55,24 @@ $(window).scroll(function () {
         });
         compteur = 1;
     }
+});
+
+
+// Swiper
+// var swiper = new Swiper('.swiper');
+
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+        delay: 4000
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+    },
 });
 
 // AOS
